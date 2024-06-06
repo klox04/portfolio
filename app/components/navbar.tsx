f379dbd0eb5d3885988f7e09e9f2243df540d6f5
Daniel <klox04>
2024-06-06 11:38:03 +0800

    navbar changes

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -10,31 +10,67 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-black p-4 relative sticky top-0 z-50">
+    <nav className="bg-black p-4 sticky top-0 z-50">
       <div className="container mx-auto px-8 flex justify-between items-center">
-        <div className="flex place-items-center gap-2 text-white text-2xl">
-          <svg xmlns="http://www.w3.org/2000/svg" 
-          fill="none" viewBox="0 0 24 24" 
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="size-6 text-gray-400">
-            <path 
-            strokeLinecap="round" 
-            strokeLinejoin="round" 
-            d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
+        <div className="flex items-center gap-2 text-white text-2xl">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 text-gray-400"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
+            />
           </svg>
-          <a href="#home" className='text-gray-400 hover:text-white hover:font-bold'>Portfolio</a>
+          <a href="#home" className="text-gray-400 hover:text-white hover:font-bold">
+            Portfolio
+          </a>
         </div>
-        <div className="lg:hidden" onClick={toggleMenu}>
-          <button className="text-white">☰</button>
+        <div className="lg:hidden">
+          <button onClick={toggleMenu} className="text-white text-3xl focus:outline-none">
+            ☰
+          </button>
         </div>
-        <div className={`flex flex-col lg:flex-row lg:items-center lg:space-x-6 ${isOpen ? 'block' : 'hidden'} lg:block`}>
-          <a href="#home" className="py-2 lg:py-0 text-gray-400 hover:text-white hover:font-bold">Home</a>
-          <a href="#about" className="py-2 lg:py-0 text-gray-400 hover:text-white hover:font-bold">About</a>
-          <a href="#project" className="py-2 lg:py-0 text-gray-400 hover:text-white hover:font-bold">Projects</a>
-          <a href="#certificates" className="py-2 lg:py-0 text-gray-400 hover:text-white hover:font-bold">Certificates</a>
-          <a href="#journal" className="py-2 lg:py-0 text-gray-400 hover:text-white hover:font-bold">Journal</a>
-          <a href="#contact" className="py-2 lg:py-0 text-gray-400 hover:text-white hover:font-bold">Contact</a>
+        <div
+          className={`flex justify-end lg:space-x-6 ${isOpen ? 'block' : 'hidden'} absolute top-16 right-0 w-44  lg:static lg:bg-transparent lg:w-auto transition-all duration-300 ease-in-out`}
+        >
+          <ul className="lg:flex lg:items-center lg:space-x-6">
+            <li>
+              <a href="#home" className="block py-2 px-4 text-gray-400 hover:text-white hover:font-bold">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="block py-2 px-4 text-gray-400 hover:text-white hover:font-bold">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#project" className="block py-2 px-4 text-gray-400 hover:text-white hover:font-bold">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#certificates" className="block py-2 px-4 text-gray-400 hover:text-white hover:font-bold">
+                Certificates
+              </a>
+            </li>
+            <li>
+              <a href="#journal" className="block py-2 px-4 text-gray-400 hover:text-white hover:font-bold">
+                Journal
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="block py-2 px-4 text-gray-400 hover:text-white hover:font-bold">
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
